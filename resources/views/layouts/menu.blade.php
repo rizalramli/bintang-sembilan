4d0e88b1917c33b85a5e6318ed80d3b953c271cc
zalramli <rizalramli993@gmail.com>
2022-11-01 17:00:20 +0700

    init migration

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,11 +1,4 @@
 
 
 
-<li class="nav-item">
-    <a href="{{ route('banks.index') }}"
-       class="nav-link {{ Request::is('banks*') ? 'active' : '' }}">
-        <p>Banks</p>
-    </a>
-</li>
-
 
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,3 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('banks', App\Http\Controllers\BankController::class);
